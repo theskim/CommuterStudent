bdc7aa4734bcf431c932a9aa4d983448a94ee41d
N00dleMaster <ayaan.path@gmail.com>
2022-08-03 21:38:47 -0400

    Move all navlinks besides logo to right side; Have them adjust/resize when screen shrinks

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -8,10 +8,12 @@ const NavBar = () => {
     return (
         <div className="nav-bar">
             <img src={CommuterLogo} alt=''/>
-            <NavLink to='/' className ="nav-link" onClick={SmoothScroll}><p>Home</p></NavLink>
-            <NavLink to='/about-us' className ="nav-link" onClick={SmoothScroll}><p>About Us</p></NavLink>
-            <NavLink to='/events' className ="nav-link" onClick={SmoothScroll}><p>Events</p></NavLink>
-            <NavLink to='/join-us' className ="nav-link" onClick={SmoothScroll}><p>Join Us</p></NavLink>
+            <span className="nav-container">
+                <NavLink to='/' className ="nav-link" onClick={SmoothScroll}><p>Home</p></NavLink>
+                <NavLink to='/about-us' className ="nav-link" onClick={SmoothScroll}><p>About Us</p></NavLink>
+                <NavLink to='/events' className ="nav-link" onClick={SmoothScroll}><p>Events</p></NavLink>
+                <NavLink to='/join-us' className ="nav-link" onClick={SmoothScroll}><p>Join Us</p></NavLink>
+            </span>
         </div>
     );
 }
