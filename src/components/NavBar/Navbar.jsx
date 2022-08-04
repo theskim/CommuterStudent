a882b739bde756e054439df14068389da91c870b
N00dleMaster <ayaan.path@gmail.com>
2022-08-04 14:07:14 -0400

    Make regular nav appear/disappear with resize and mobile nav disappear/appear

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { NavLink } from "react-router-dom";
 import { SmoothScroll } from "./SmoothScroll";
 import './Navbar.css';
 import { CommuterLogo } from "../../assets"; 
 
 const NavBar = () => {
+
+    const [mobileNav, setMobileNav] = useState(false);
+    
+
+    useEffect(() => {
+        const handleResize = () => {
+            if(window.innerWidth < 850) {
+                setMobileNav(true);
+            }  else {
+                setMobileNav(false);
+            }
+        }
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
+    })
+
     return (
         <div className="nav-bar">
             <img src={CommuterLogo} alt=''/>
-            <span className="nav-container">
+            <span className={`nav-container ${mobileNav ? "ohWowLookItsGone" : ""}`}>
+                <NavLink to='/' className ="nav-link" onClick={SmoothScroll}><p>Home</p></NavLink>
+                <NavLink to='/about-us' className ="nav-link" onClick={SmoothScroll}><p>About Us</p></NavLink>
+                <NavLink to='/events' className ="nav-link" onClick={SmoothScroll}><p>Events</p></NavLink>
+                <NavLink to='/join-us' className ="nav-link" onClick={SmoothScroll}><p>Join Us</p></NavLink>
+            </span>
+            <span className={`nav-mobile-container ${mobileNav ? "" : "ohWowLookItsGone"}`}>
                 <NavLink to='/' className ="nav-link" onClick={SmoothScroll}><p>Home</p></NavLink>
                 <NavLink to='/about-us' className ="nav-link" onClick={SmoothScroll}><p>About Us</p></NavLink>
                 <NavLink to='/events' className ="nav-link" onClick={SmoothScroll}><p>Events</p></NavLink>
