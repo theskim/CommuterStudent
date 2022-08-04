cafa94010f97ebaa1d932406d7f3f411be93e17b
N00dleMaster <ayaan.path@gmail.com>
2022-08-04 15:30:19 -0400

    Add MobileMenu element to Navbar.jsx and tweak navbar height in Navbar.css

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { SmoothScroll } from "./SmoothScroll";
 import './Navbar.css';
 import { CommuterLogo } from "../../assets"; 
+import MobileMenu from "./MobileMenu/MobileMenu";
 
 const NavBar = () => {
 
@@ -32,10 +33,7 @@ const NavBar = () => {
                 <NavLink to='/join-us' className ="nav-link" onClick={SmoothScroll}><p>Join Us</p></NavLink>
             </span>
             <span className={`nav-mobile-container ${mobileNav ? "" : "ohWowLookItsGone"}`}>
-                <NavLink to='/' className ="nav-link" onClick={SmoothScroll}><p>Home</p></NavLink>
-                <NavLink to='/about-us' className ="nav-link" onClick={SmoothScroll}><p>About Us</p></NavLink>
-                <NavLink to='/events' className ="nav-link" onClick={SmoothScroll}><p>Events</p></NavLink>
-                <NavLink to='/join-us' className ="nav-link" onClick={SmoothScroll}><p>Join Us</p></NavLink>
+                <MobileMenu />
             </span>
         </div>
     );
