4f029631f321812b6002c75e3039c151e7e438d6
N00dleMaster <ayaan.path@gmail.com>
2022-08-05 15:50:19 -0400

    Realize I had a smooth-brained moment and move code to handle resizing to css file

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { SmoothScroll } from "./SmoothScroll";
 import './Navbar.css';
@@ -7,32 +7,16 @@ import MobileMenu from "./MobileMenu/MobileMenu";
 
 const NavBar = () => {
 
-    const [mobileNav, setMobileNav] = useState(false);
-    
-
-    useEffect(() => {
-        const handleResize = () => {
-            if(window.innerWidth < 850) {
-                setMobileNav(true);
-            }  else {
-                setMobileNav(false);
-            }
-        }
-
-        handleResize();
-        window.addEventListener("resize", handleResize);
-    })
-
     return (
         <div className="nav-bar">
             <img src={CommuterLogo} alt=''/>
-            <span className={`nav-container ${mobileNav ? "ohWowLookItsGone" : ""}`}>
+            <span className="nav-container">
                 <NavLink to='/' className ="nav-link" onClick={SmoothScroll}><p>Home</p></NavLink>
                 <NavLink to='/about-us' className ="nav-link" onClick={SmoothScroll}><p>About Us</p></NavLink>
                 <NavLink to='/events' className ="nav-link" onClick={SmoothScroll}><p>Events</p></NavLink>
                 <NavLink to='/join-us' className ="nav-link" onClick={SmoothScroll}><p>Join Us</p></NavLink>
             </span>
-            <span className={`nav-mobile-container ${mobileNav ? "" : "ohWowLookItsGone"}`}>
+            <span className="nav-mobile-container">
                 <MobileMenu />
             </span>
         </div>
