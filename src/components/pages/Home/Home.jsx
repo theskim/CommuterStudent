702c7580139480ef4052c0f9507092b886e35741
skim1601 <top030216@gmail.com>
2022-08-06 20:39:49 -0400

    discord link adjustment

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
           <h1>Skule™ Commuter Student Project Directorship</h1>
           <h2>Contact Us</h2>
           <div className="home__buttons">
-            <SocialMediaButton source={DiscordLogo} link="https://discord.gg/nf4va7CR"/>
+            <SocialMediaButton source={DiscordLogo} link="https://discord.gg/yph2EjkbhW"/>
             <SocialMediaButton source={InstagramLogo} link="https://www.instagram.com/skule.commuters/"/>
           </div>
         </div>
