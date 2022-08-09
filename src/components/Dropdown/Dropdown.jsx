8b269f4f1f6e647c044dbd6cdd9d8d1432164cb6
N00dleMaster <ayaan.path@gmail.com>
2022-08-09 14:42:14 -0400

    improve html structure

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -4,7 +4,12 @@ const Dropdown = (props) => {
 
     return (
         <div className="dropdown">
-            <button className="dropdown__header">{props.title}</button>
+            <div className="dropdown__header">
+                <span className="dropdown__header__caret">
+                    <img src="https://img.icons8.com/material/24/000000/down-arrow.png" alt="down arrow" />
+                </span>
+                <div className="dropdown__header__title"><h3>{props.title}</h3></div>
+            </div>
             <div className="dropdown__content">
                 {props.content.map((item) => {
                     return <div className="dropdown__content__item">{item}</div>
