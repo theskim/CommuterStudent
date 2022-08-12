974f145727924c89b73b1cb9850a2f4ffb73ecf2
skim1601 <top030216@gmail.com>
2022-08-12 16:29:22 -0400

    minor adjustments

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+    <div className="footer">
+        Ⓒ Skule Commuter Students Directorship 2022. All rights reserved.
+    </div>   
+}
+
+export default Footer;
