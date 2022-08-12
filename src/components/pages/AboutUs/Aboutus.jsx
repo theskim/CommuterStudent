f602e4784ec9c75a9afe30470adada636a029e47
skim1601 <top030216@gmail.com>
2022-08-12 16:21:54 -0400

    margin adjustments regarding Cards and Member Profile

--- src/components/pages/AboutUs/Aboutus.jsx
+++ src/components/pages/AboutUs/Aboutus.jsx
@@ -11,7 +11,6 @@ const AboutUs = () => {
           <div className="section-title">
             <h2>About Us</h2>
           </div>
-          <div className="about-us__header">
             <Cards>
               <Card 
                 title="Our Mission" 
@@ -27,7 +26,6 @@ const AboutUs = () => {
                         Additionally, we have monthly events. To learn more, check out the Events page."
               />
             </Cards>
-          </div>
           
           <div className="section-title">
             <h2>Meet Our Team</h2>
