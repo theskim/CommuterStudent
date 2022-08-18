041f5b040df3dc7fff396b22c839d1064f8bae32
Ayaan Pathan <noodlemaster@pop-os.localdomain>
2022-08-18 15:01:40 -0400

    Make  rudimentary desktop layout

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,18 +1,23 @@
 import SocialMediaButton from "./SocialMediaButton";
-import { InstagramLogo, DiscordLogo } from "../../../assets";
+import { InstagramLogo, DiscordLogo, CommuterBanner } from "../../../assets";
 import './Home.css';
 
 const Home = () => {
     return (
-      <div className="custom-background">
-        <div className="home">
-          <h1>Skule™ Commuter Student Project Directorship</h1>
-          <div className="home__buttons">
-            <SocialMediaButton source={DiscordLogo} link="https://discord.gg/yph2EjkbhW"/>
-            <SocialMediaButton source={InstagramLogo} link="https://www.instagram.com/skule.commuters/"/>
-          </div>
-        </div>
-      </div>
+		<div className="home-banner">
+			<div className="home-banner__img">
+				<img src={CommuterBanner} alt="" />
+			</div>
+			<div className="home-banner__content">
+				<div className="home-banner__content__title">
+					<h1>Commuter Project Directorship</h1>
+				</div>
+				<div className="home-banner__content__icons">
+					<SocialMediaButton source={DiscordLogo} link="https://discord.gg/yph2EjkbhW"/>
+					<SocialMediaButton source={InstagramLogo} link="https://www.instagram.com/skule.commuters/"/>
+				</div>
+			</div>
+		</div>
     );
   }
   
