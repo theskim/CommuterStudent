b617ea17266da1abe676893db33d0842cff25d57
Ayaan Pathan <ayaan.path@gmail.com>
2022-08-20 12:27:53 -0400

    Add mobile breakpoints and further refine desktop breakpoints

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -18,6 +18,8 @@ const Home = () => {
 					<SocialMediaButton source={EmailLogo} link="mailto:webmaster@commuter.skule.ca"/>
 				</div>
 			</div>
+			{/* This is only for the mobile layout. It turns transluscent when mobile layout kicks in. */}
+			<div className="home-banner__background"></div> 
 		</div>
     );
   }
