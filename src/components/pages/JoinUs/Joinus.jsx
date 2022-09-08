3f644720b57659a58146e5e7cac05159945891d3
Ayaan Pathan <ayaan.path@gmail.com>
2022-09-07 21:29:42 -0400

    Add embedded google membership form

--- src/components/pages/JoinUs/Joinus.jsx
+++ src/components/pages/JoinUs/Joinus.jsx
@@ -1,11 +1,18 @@
 import Footer from "../../Footer/Footer";
 import Background from "../../Background/Background";
+import "./Joinus.css";
 
 const JoinUs = () => {
     return (
       <Background>
         <div className="join-us">
-          <h3>Membership Form</h3>
+            <div className="section-title">
+                  <h1>Membership From</h1>
+            </div>
+
+            <div className="formEmbed">
+              <iframe title="formEmbed" src="https://docs.google.com/forms/d/e/1FAIpQLSdRJYQjh4D_NohNCoTX7emkSbLzkBCnO6Kjkrs0o3Eu9wrBAw/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
         </div>
         <Footer/>
       </Background>
