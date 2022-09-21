916087378387cc32e4eea4fc48a5a74745f9f1ee
Ayaan Pathan <ayaan.path@gmail.com>
2022-09-21 13:11:33 -0400

    Add second form to join-us page

--- src/components/pages/JoinUs/Joinus.jsx
+++ src/components/pages/JoinUs/Joinus.jsx
@@ -13,6 +13,16 @@ const JoinUs = () => {
             <div className="formEmbed">
               <iframe title="formEmbed" src="https://docs.google.com/forms/d/e/1FAIpQLSdRJYQjh4D_NohNCoTX7emkSbLzkBCnO6Kjkrs0o3Eu9wrBAw/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             </div>
+            
+            
+            <div className="section-title">
+                  <h1>Commuter Buddy Program Signup</h1>
+                  <p><strong>Don't hesitate! Sign up now!</strong> Sign ups close <strong>September 30th</strong> (subject to change)</p>
+            </div>
+
+            <div className="formEmbed">
+              <iframe title="formEmbed" src="https://docs.google.com/forms/d/e/1FAIpQLSdRJYQjh4D_NohNCoTX7emkSbLzkBCnO6Kjkrs0o3Eu9wrBAw/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
         </div>
         <Footer/>
       </Background>
