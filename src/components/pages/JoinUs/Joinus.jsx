96f044e75261a6dbf77644f6b1a3cb6980981825
Ayaan Pathan <ayaan.path@gmail.com>
2022-09-21 14:16:05 -0400

    Dynamically resize forms as page resizes, and add space between the two forms

--- src/components/pages/JoinUs/Joinus.jsx
+++ src/components/pages/JoinUs/Joinus.jsx
@@ -7,13 +7,15 @@ const JoinUs = () => {
       <Background>
         <div className="join-us">
             <div className="section-title">
-                  <h1>Membership From</h1>
+                  <h1>Membership Form</h1>
             </div>
 
             <div className="formEmbed">
-              <iframe title="formEmbed" src="https://docs.google.com/forms/d/e/1FAIpQLSdRJYQjh4D_NohNCoTX7emkSbLzkBCnO6Kjkrs0o3Eu9wrBAw/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                <div className="formContainer">
+                  <iframe title="formEmbed" src="https://docs.google.com/forms/d/e/1FAIpQLSdRJYQjh4D_NohNCoTX7emkSbLzkBCnO6Kjkrs0o3Eu9wrBAw/viewform?embedded=true" width="100%" height="650" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                </div>
             </div>
-            
+
             
             <div className="section-title">
                   <h1>Commuter Buddy Program Signup</h1>
@@ -21,7 +23,9 @@ const JoinUs = () => {
             </div>
 
             <div className="formEmbed">
-              <iframe title="formEmbed" src="https://docs.google.com/forms/d/e/1FAIpQLSdRJYQjh4D_NohNCoTX7emkSbLzkBCnO6Kjkrs0o3Eu9wrBAw/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                <div className="formContainer">
+                  <iframe title="formEmbed" src="https://docs.google.com/forms/d/e/1FAIpQLSdRJYQjh4D_NohNCoTX7emkSbLzkBCnO6Kjkrs0o3Eu9wrBAw/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                </div>
             </div>
         </div>
         <Footer/>
