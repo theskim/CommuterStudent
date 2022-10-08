f22f024cc7893529c27a0cd28646b4d34e797204
Ayaan Pathan <ayaan.path@gmail.com>
2022-10-08 17:16:39 -0400

    Remove buddy program form since sign ups are over

--- src/components/pages/JoinUs/Joinus.jsx
+++ src/components/pages/JoinUs/Joinus.jsx
@@ -15,18 +15,6 @@ const JoinUs = () => {
                   <iframe title="member" src="https://docs.google.com/forms/d/e/1FAIpQLSdRJYQjh4D_NohNCoTX7emkSbLzkBCnO6Kjkrs0o3Eu9wrBAw/viewform?embedded=true" width="100%" height="650" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                 </div>
             </div>
-
-            
-            <div className="section-title">
-                  <h1>Commuter Buddy Program Signup</h1>
-                  <p><strong>Don't hesitate! Sign up now!</strong> Sign ups close <strong>September 30th</strong> (subject to change)</p>
-            </div>
-
-            <div className="formEmbed">
-                <div className="formContainer">
-                  <iframe title="buddy" src="https://docs.google.com/forms/d/e/1FAIpQLSdM6RW_y5PbuC5D5Vm5eg8fyALP8aV27x-nVyU_wVvAyhjL6w/viewform?embedded=true" width="100%" height="650" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-                </div>
-            </div>
         </div>
         <Footer/>
       </Background>
